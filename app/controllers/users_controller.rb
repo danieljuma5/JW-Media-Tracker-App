a707e9aab4ca88ad669375bbfc5c94a7c328a914
danieljuma5 <jumadaniel275@gmail.com>
2023-07-04 21:59:24 +0300

    Added /me path

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :authorize, only: :show
+  skip_before_action :authorize,only: :create
 rescue_from ActiveRecord::RecordInvalid, with: :render_user_invalid_unprocessable_entity
   def create
        user = User.create!(user_params)
@@ -6,8 +8,15 @@ rescue_from ActiveRecord::RecordInvalid, with: :render_user_invalid_unprocessabl
         session[:user_id] ||= user.id
         render json: user, status: :created
   end
+   def show
+    user = User.find_by(id: session[:user_id])
+      render json: user
+  end
 
   private
+  def authorize
+    render json: {errors: ["Not Authorized"]},status: :unauthorized unless session.include? :user_id
+  end
   def render_user_invalid_unprocessable_entity(invalid)
     render json: { errors: [invalid.record.errors.full_messages] }, status: :unprocessable_entity
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :posts
   resources :comments, only:[:index,:update,:destroy]
   post "/signup", to: "users#create"
+  get "/me", to: "users#show"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
