d2b40bfa2aac192698dc27be9383498cee39e663
danieljuma5 <jumadaniel275@gmail.com>
2023-07-07 07:58:37 +0300

    Fetched comments page data

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,4 +1,5 @@
 class PostSerializer < ActiveModel::Serializer
   attributes :id, :title, :image_url, :description, :is_liked, :upvotes, :downvotes
   has_one :user
+  has_many :comments
 end
--- client/src/Components/CommentPage.js
+++ client/src/Components/CommentPage.js
@@ -1,6 +1,16 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
+// import axios from 'axios';
 
 const CommentPage = () => {
+  const [data,setData] = useState([])
+  const {postId} = useParams()
+  console.log(postId)
+  useEffect(() => {
+    axios.get(`/posts/${postId}`)
+    .then((r) => console.log(r))
+  },[])
   return (
     <div>CommentPage</div>
   )
