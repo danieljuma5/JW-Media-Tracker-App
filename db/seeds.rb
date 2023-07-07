4c0d6324aa629467bb507c121c8e023cb032560d
danieljuma5 <jumadaniel275@gmail.com>
2023-07-08 00:28:45 +0300

    Fixed seed bug

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,10 +26,10 @@ forum_posts = ForumPost.create!(
 )
 end
 20.times do
-  forum_relies=ForumReply.create!(
+  forum_replies=ForumReply.create!(
     body:Faker::Quote.most_interesting_man_in_the_world,
     user_id:rand(1..5),
-    post_id:rand(1..10),
+    forum_post_id:rand(1..10),
   )
 end
 
