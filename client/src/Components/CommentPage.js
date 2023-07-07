b6d11cdb71cb8cca8c98ffc48602543f8b8fb9c2
danieljuma5 <jumadaniel275@gmail.com>
2023-07-07 09:03:34 +0300

    Fixed commment page render bug

--- client/src/Components/CommentPage.js
+++ client/src/Components/CommentPage.js
@@ -1,36 +1,37 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import CommentPageCard from './CommentPageCard';
 import CommentSection from './CommentSection';
-// import axios from 'axios';
 
 const CommentPage = () => {
-  const [posts,setPosts] = useState([])
-  const {postId} = useParams()
-  console.log(postId)
+  const [isLoading, setIsLoading] = useState(true);
+  const [posts, setPosts] = useState([]);
+  const { postId } = useParams();
+
   useEffect(() => {
     axios.get(`/posts/${postId}`)
-    .then((r) => setPosts(r.data))
-  },[])
-  console.log(posts)
+      .then((response) => {
+        setPosts(response.data);
+        setIsLoading(false); // Mark the loading as false once the posts are fetched
+      })
+      .catch((error) => {
+        console.log(error);
+        setIsLoading(false); // In case of an error, mark the loading as false to handle the error state
+      });
+  }, [postId]);
+
+  // Render loading state until the posts are fetched
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <>
-    <CommentPageCard post={posts}/>
-    <CommentSection post={posts}/>
-    
+      <CommentPageCard post={posts} />
+      <CommentSection />
     </>
-  )
-}
+  );
+};
 
-export default CommentPage
-
-
-
-
- 
-
-
-
-
-
+export default CommentPage;
