b5443dd4b92abf42b88a890425b8d908636c3822
danieljuma5 <126584149+danieljuma5@users.noreply.github.com>
2024-01-12 15:53:28 +0300

    Update seeds.rb
    Fixed user create logic issue

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,37 +1,35 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
 puts 'seeding...'
-$password_var = Faker::Alphanumeric.alphanumeric(number: 10)
 
-5.times do 
+# Create users
+5.times do
   user = User.create!(
-    username:Faker::Internet.username,
-    email:Faker::Internet.email,
-    password_digest:BCrypt::Password.create("#$password_var"),
-    avatar_url:Faker::LoremFlickr.grayscale_image
+    username: Faker::Internet.username,
+    email: Faker::Internet.email,
+    password_digest: BCrypt::Password.create(Faker::Alphanumeric.alphanumeric(number: 10)),
+    avatar_url: Faker::LoremFlickr.grayscale_image
   )
 end
 
-10.times do 
-forum_posts = ForumPost.create!(
-  title:Faker::Movie.title,
-  category:Faker::Book.genre,
-  description:Faker::Lorem.paragraph(sentence_count: 2, supplemental: false, random_sentences_to_add: 4),
-  user_id:rand(1..7)
-)
+# Create forum posts
+10.times do
+  user = User.all.sample # Fetch a random user
+  forum_post = ForumPost.create!(
+    title: Faker::Movie.title,
+    category: Faker::Book.genre,
+    description: Faker::Lorem.paragraph(sentence_count: 2, supplemental: false, random_sentences_to_add: 4),
+    user: user
+  )
 end
+
+# Create forum replies
 20.times do
-  forum_replies=ForumReply.create!(
-    body:Faker::Quote.most_interesting_man_in_the_world,
-    user_id:rand(1..5),
-    forum_post_id:rand(1..10),
+  user = User.all.sample # Fetch a random user
+  forum_post = ForumPost.all.sample # Fetch a random forum post
+  forum_reply = ForumReply.create!(
+    body: Faker::Quote.most_interesting_man_in_the_world,
+    user: user,
+    forum_post: forum_post
   )
 end
-
 
 puts '✅ Done seeding!'
